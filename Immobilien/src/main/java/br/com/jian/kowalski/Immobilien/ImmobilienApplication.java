043187edc8c8ffe91a86292ec549fe6b4602e968
jiankowalski <jean_kowalski@hotmail.com>
2017-07-25 22:08:52 -0300

    inicial commit

---
+++ Immobilien/src/main/java/br/com/jian/kowalski/Immobilien/ImmobilienApplication.java
@@ -0,0 +1,12 @@
+package br.com.jian.kowalski.Immobilien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmobilienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmobilienApplication.class, args);
+	}
+}
---
+++ Immobilien/src/test/java/br/com/jian/kowalski/Immobilien/ImmobilienApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.jian.kowalski.Immobilien;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmobilienApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
